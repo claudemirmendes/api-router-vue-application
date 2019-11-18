708becdf9a5c8390ea81a459bb22d71e639ab444
claudemir <claudemir@QPRHLDTICN08373.local>
2019-11-18 17:58:31 -0300

    teste

--- app/controllers/private/base_controller.rb
+++ app/controllers/private/base_controller.rb
@@ -1,2 +1,15 @@
 class Private::BaseController < ApplicationController
+    before_action :validate_token
+
+    # def validate_token
+    #   user = User.where(token: params[:token]).last unless params[:token].nil?
+    #   if (user.nil?)
+    #      render json: {message: "token expired"}, status: 401
+    #   end
+    # end
+
+    def validate_token
+        teste_json = {"teste": "teste"}
+        render json: teste_json
+    end
 end
