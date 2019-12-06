ad2b31f4e37d8245e08f0b94c0b5ed8aa5030164
claudemir <claudemirmendes>
2019-12-06 08:36:35 -0300

    corrigindo erro de login

--- app/models/auth_token.rb
+++ app/models/auth_token.rb
@@ -3,7 +3,7 @@
 class AuthToken < ApplicationRecord
   def self.generate_token(user)
     auth_token = AuthToken.where(user_id: user.id)
-    if auth_token.nil?
+    if auth_token.empty?
       hash_token = SecureRandom.uuid
       date = DateTime.now + 1.year
       auth_token = AuthToken.create(token: hash_token, user_id: user.id, expired_date: date)
